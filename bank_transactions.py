e07e6b6052e4e462510070f89603e6191eab9079
Curtis Ainslie <cainslie@rrc.ca>
2024-10-06 17:00:20 -0500

    Added a loop for the ATM menu selection and the menu now holds for 3 seconds then resets to the beginning

--- bank_transactions.py
+++ bank_transactions.py
@@ -5,6 +5,8 @@ Date: 2024-10-05
 Usage: bank_transaction.py
 """
 from random import randint
+import os
+from time import sleep
 
 menu_options = ['D', 'W', 'Q']
 
@@ -21,70 +23,79 @@ invalid_line = "INVALID SELECTION"
 insufficient_funds = "INSUFFICIENT FUNDS"
 user_selection = ""
 
-# Different menu pop ups
-atm_selection_menu = [menu_line_break, menu_bank_name,
-                    (f"Your current balance is: ${current_balance}"),
-                    menu_deposit, menu_withdraw, menu_quit, 
-                    menu_line_break]
+# ATM loop
+while user_selection.upper() != 'Q':
 
-invalid_selection = [menu_line_break, invalid_line, menu_line_break]
-
-insufficient_funds_menu = [menu_line_break, insufficient_funds, 
-                            menu_line_break]
-
-# Prints out the menu
-for menu_lines in atm_selection_menu:
-    print("{:^40}".format(menu_lines))
-
-user_selection = input("Enter your selection: ").upper()
-
-# Selection options
-
-# Deposit option
-if user_selection == menu_options[0]:
-    user_deposit = input("Enter amount of transaction: ")
-    user_deposit = float(user_deposit)
-
-    current_balance = current_balance + user_deposit
-    
-    view_current_balance = [menu_line_break, \
-                        (f"Your current balance is: ${current_balance}"), \
+    # Different menu pop ups
+    atm_selection_menu = [menu_line_break, menu_bank_name,
+                        f"Your current balance is: ${current_balance}",
+                        menu_deposit, menu_withdraw, menu_quit, 
                         menu_line_break]
 
-    for view_current_balance_lines in view_current_balance:
-        print("{:^40}".format(view_current_balance_lines))
+    invalid_selection = [menu_line_break, invalid_line, menu_line_break]
 
-# Withdraw option
-elif user_selection == menu_options[1]:
-    user_withdraw = input("Enter amount of transaction: ")
-    user_withdraw = float(user_withdraw)
+    insufficient_funds_menu = [menu_line_break, insufficient_funds, 
+                                menu_line_break]
 
-    current_balance = current_balance - user_withdraw
+    # Prints out the menu
+    for menu_lines in atm_selection_menu:
+        print("{:^40}".format(menu_lines))
 
-    if current_balance < 0:
-        current_balance = current_balance + user_withdraw
+    user_selection = input("Enter your selection: ").upper()
 
-        for insufficient_funds_lines in insufficient_funds_menu:
-            print("{:^40}".format(insufficient_funds_lines))
+    # Selection options
 
-    else:
-        view_current_balance = [menu_line_break, \
-                            (f"Your current balance is: ${current_balance}"), \
-                            menu_line_break]
+    # Deposit option
+    if user_selection == menu_options[0]:
+        user_deposit = input("Enter amount of transaction: ")
+        user_deposit = float(user_deposit)
+
+        current_balance = current_balance + user_deposit
+        
+        view_current_balance = [menu_line_break,
+                                f"Your current balance is: ${current_balance}",
+                                menu_line_break]
 
         for view_current_balance_lines in view_current_balance:
             print("{:^40}".format(view_current_balance_lines))
 
-# Quit option
-elif user_selection == menu_options[2]:
-    view_current_balance = [menu_line_break, \
-                            (f"Your current balance is: ${current_balance}"), \
-                            menu_line_break]
-    
-    for view_current_balance_lines in view_current_balance:
-        print("{:^40}".format(view_current_balance_lines))
+    # Withdraw option
+    elif user_selection == menu_options[1]:
+        user_withdraw = input("Enter amount of transaction: ")
+        user_withdraw = float(user_withdraw)
 
-# Invalid option
-else:
-    for invalid_selection_lines in invalid_selection:
-        print("{:^40}".format(invalid_selection_lines))
+        current_balance = current_balance - user_withdraw
+
+        if current_balance < 0:
+            current_balance = current_balance + user_withdraw
+
+            for insufficient_funds_lines in insufficient_funds_menu:
+                print("{:^40}".format(insufficient_funds_lines))
+
+        else:
+            view_current_balance = [menu_line_break,
+                                f"Your current balance is: ${current_balance}",
+                                menu_line_break]
+
+            for view_current_balance_lines in view_current_balance:
+                print("{:^40}".format(view_current_balance_lines))
+
+    # Quit option
+    elif user_selection == menu_options[2]:
+        view_current_balance = [menu_line_break,
+                                f"Your current balance is: ${current_balance}",
+                                menu_line_break]
+        
+        for view_current_balance_lines in view_current_balance:
+            print("{:^40}".format(view_current_balance_lines))
+
+    # Invalid option
+    else:
+        for invalid_selection_lines in invalid_selection:
+            print("{:^40}".format(invalid_selection_lines))
+
+    # Pause the script for the specified number of seconds
+    sleep(3)
+
+    # Clear the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
